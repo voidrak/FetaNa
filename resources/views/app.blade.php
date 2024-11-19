9fef9616e8a8bc2f06bc4ba79ec9cdbdcd012695
RohaTech <raklovetocode@gmail.com>
2024-11-19 10:27:10 +0300

    feat: add favicon to the application

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="/icon.png" type="image/x-icon">
     <title>FetaNa</title>
     @vite('resources/js/app.js')
     @inertiaHead
